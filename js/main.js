21b11b411b02997eaa121575be10b244249ac3cd
Jacqueline Estrada <jackie@Jacquelines-MacBook-Air.local>
2017-12-21 10:33:24 -0800

    Win logic

--- js/main.js
+++ js/main.js
@@ -4,12 +4,9 @@ var playerColors = {
     'null': 'white'
 };
 
-
 var board, turn, win;
 
-
 var msgEl = document.getElementById('message');
-
 
 document.querySelector('table').addEventListener('click', function(evt) {
     var colIdx = parseInt(evt.target.getAttribute('data-col'));
@@ -21,13 +18,11 @@ document.querySelector('table').addEventListener('click', function(evt) {
     render();
 });
 
-
 document.querySelector('button').addEventListener('click', function(evt) {
     console.log('hello!!!!!!!!')
     initialize();
     render();
 });
-
 
 function initialize() {
     board = [
@@ -42,7 +37,6 @@ function initialize() {
     turn = 1;
     win = null;
 }
-
 
 function render() {
     board.forEach(function(colArr, idx) {
@@ -59,19 +53,52 @@ function render() {
     } 
 }
 
-
 function getWinner() {
-     /* var winner = null;
-    for (var col = 0, i < winner.length; i++) {
-        winner = getColumnWin(colIdx);
+     var winner = null;
+    for (var col = 0; col < board.length; col++) {
+        winner = getColumnWinner(col);
     if (winner) break;
-}
-    return winner;
-        }
-    for( var i = 0; i < column.length; i++) {
-
     }
-    */
+    return winner;
 }
+
+function getColumnWinner(colIdx) {
+    var winner; 
+    var colArr = board[colIdx];
+    winner = checkVert(colArr);
+    if (winner) return winner;
+    winner = checkHoriz(colIdx);
+    if (winner) return winner;
+    winner = checkDiag(colIdx, 1);
+    if (winner) return winner;
+    winner = checkDiag(colIdx, -1);
+    if (winner) return winner;
+    return null;
+}
+
+function checkVert(colArr) {
+    for (var row = 0; row < 3; row++) {
+        if (Math.abs(colArr[row] + colArr[row + 1] + colArr[row + 2] + colArr[row + 3]) === 4) return colArr[row];
+    }
+    return null;
+}
+
+function checkHoriz(colIdx) {
+    if (colIdx > 3) return null;
+    for (var row = 0; row < 7; row++) {
+        if (Math.abs(board[colIdx][row] + board[colIdx + 1][row] + board[colIdx + 2][row] + board[colIdx + 3][row]) === 4) return board[colIdx][row];
+    }
+    return null;
+}
+
+function checkDiag(colIdx, offset) {
+    if (colIdx > 3) return null;
+    for (var row = 0; row < 7; row++) {
+        if ((offset === 1 && row > 2) || (offset === -1 && row < 3)) break;
+        if (Math.abs(board[colIdx][row] + board[colIdx + 1][row + offset] + board[colIdx + 2][row + offset * 2] + board[colIdx + 3][row + offset * 3]) === 4) return board[colIdx][row];
+    }
+    return null;
+}
+
 initialize();
 render();
