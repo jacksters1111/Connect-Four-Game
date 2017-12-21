eebcde163dbd244df6fda36a9a978246649e4712
Jackie Estrada <33438947+jacksters1111@users.noreply.github.com>
2017-12-21 10:03:20 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,44 +1,29 @@
-/*----- constants -----*/
 var playerColors = {
     '1': 'blue',
     '-1': 'red',
     'null': 'white'
 };
 
-
-/*----- app's state (variables) -----*/
 var board, turn, win;
 
-/*----- cached element references -----*/
-// a message telling whos turn it is whether its player 1 or 2's turn
 var msgEl = document.getElementById('message');
 
-
-/*----- event listeners -----*/
-// when game starts it will be blank until someone clicks a square
-//when one square is clicked the last one on the squares will be
-// filled in.
 document.querySelector('table').addEventListener('click', function(evt) {
     var colIdx = parseInt(evt.target.getAttribute('data-col'));
     var column = board[colIdx];
     if (!column.includes(null)) return;
-    // task: using clicked element id update the correct element in my board array
     column[column.indexOf(null)] = turn;
     turn *= -1;
-    // win = getWinner();
+    win = getWinner();
     render();
 });
 
-
-// when restart game button is clicked clear the game
 document.querySelector('button').addEventListener('click', function(evt) {
     console.log('hello!!!!!!!!')
     initialize();
     render();
 });
 
-
-/*----- functions -----*/
 function initialize() {
     board = [
         [null, null, null, null, null, null],
@@ -65,11 +50,54 @@ function render() {
     } else {
         msgEl.innerHTML = `It's Player ${playerColors[turn].toUpperCase()}'s Turn!`;
 
-    }
+    } 
 }
 
+function getWinner() {
+     var winner = null;
+    for (var col = 0; col < board.length; col++) {
+        winner = getColumnWinner(col);
+    if (winner) break;
+    }
+    return winner;
+}
 
-/*----- win logic -----*/
+function getColumnWinner(colIdx) {
+    var winner; 
+    var colArr = board[colIdx];
+    winner = checkVert(colArr);
+    if (winner) return winner;
+    winner = checkHoriz(colIdx);
+    if (winner) return winner;
+    winner = checkDiag(colIdx, 1);
+    if (winner) return winner;
+    winner = checkDiag(colIdx, -1);
+    if (winner) return winner;
+    return null;
+}
 
+function checkVert(colArr) {
+    for (var row = 0; row < 3; row++) {
+        if (Math.abs(colArr[row] + colArr[row + 1] + colArr[row + 2] + colArr[row + 3]) === 4) return colArr[row];
+    }
+    return null;
+}
+
+function checkHoriz(colIdx) {
+    if (colIdx > 3) return null;
+    for (var row = 0; row < 7; row++) {
+        if (Math.abs(board[colIdx][row] + board[colIdx + 1][row] + board[colIdx + 2][row] + board[colIdx + 3][row]) === 4) return board[colIdx][row];
+    }
+    return null;
+}
+
+function checkDiag(colIdx, offset) {
+    if (colIdx > 3) return null;
+    for (var row = 0; row < 7; row++) {
+        if ((offset === 1 && row > 2) || (offset === -1 && row < 3)) break;
+        if (Math.abs(board[colIdx][row] + board[colIdx + 1][row + offset] + board[colIdx + 2][row + offset * 2] + board[colIdx + 3][row + offset * 3]) === 4) return board[colIdx][row];
+    }
+    return null;
+}
 initialize();
 render();
